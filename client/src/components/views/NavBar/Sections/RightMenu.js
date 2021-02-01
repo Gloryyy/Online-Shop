f621fcfca2c0834ab7e51bda4d62daf5f1ff7568
gloryyy <majdwarrrak@gmail.com>
2021-02-01 20:18:34 +0100

    Adding PayPal payment

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,16 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { Menu } from "antd";
-import axios from "axios";
+import { Menu, Icon, Badge } from "antd";
+import Axios from "axios";
 import { USER_SERVER } from "../../../Config";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function RightMenu(props) {
   const user = useSelector((state) => state.user);
+  const { userData } = user;
 
   const logoutHandler = () => {
-    axios.get(`${USER_SERVER}/logout`).then((response) => {
+    Axios.get(`${USER_SERVER}/logout`).then((response) => {
       if (response.status === 200) {
         props.history.push("/login");
       } else {
@@ -23,21 +24,42 @@ function RightMenu(props) {
     return (
       <Menu mode={props.mode}>
         <Menu.Item key="mail">
-          <a href="/login">Signin</a>
+          <Link to="/login">Signin</Link>
         </Menu.Item>
         <Menu.Item key="app">
-          <a href="/register">Signup</a>
+          <Link to="/register">Signup</Link>
         </Menu.Item>
       </Menu>
     );
   } else {
     return (
       <Menu mode={props.mode}>
-        <Menu.Item key="upload">
-          <a href="/product/upload">Upload</a>
+        <Menu.Item key="history">
+          <Link to="/history">History</Link>
         </Menu.Item>
+
+        <Menu.Item key="upload">
+          <Link to="/product/upload">Upload</Link>
+        </Menu.Item>
+
+        <Menu.Item key="cart">
+          <Badge count={userData && userData.cart.length}>
+            <Link
+              to="/user/cart"
+              style={{ marginRight: "-22px", color: "#667777" }}
+            >
+              <Icon
+                type="shopping-cart"
+                style={{ fontSize: "30px", marginBottom: "4px" }}
+              />
+            </Link>
+          </Badge>
+        </Menu.Item>
+
         <Menu.Item key="logout">
-          <a onClick={logoutHandler}>Logout</a>
+          <Link to="/login" onClick={logoutHandler}>
+            Logout
+          </Link>
         </Menu.Item>
       </Menu>
     );
